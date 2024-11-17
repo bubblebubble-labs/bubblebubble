40d307a5d8d0eb55fbea77deb6b27a72c9064e6d
CheolheeLee0 <wks0968@gmail.com>
2024-11-17 13:56:05 +0900

    chore: 주석 처리

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -6,19 +6,8 @@ export const metadata = {
 import Hero from '@/components/hero'
 import Clients from '@/components/clients'
 import Features from '@/components/features'
-import Features02 from '@/components/features-02'
-import Features03 from '@/components/features-03'
 import Features05 from '@/components/features-05'
-import Keyword from '@/components/keyword'
-
-import TestimonialsCarousel from '@/components/testimonials-carousel'
-import Features04 from '@/components/features-04'
-import Pricing from './pricing-section'
-import Testimonials from '@/components/testimonials'
-import Cta from '@/components/cta'
-import Customers from '@/components/customers'
-import Faqs from '@/components/faqs'
-import Cta03 from '@/components/cta-03'
+import Keyword from '@/components/Keyword'
 
 export default function Home() {
   return (
@@ -28,16 +17,6 @@ export default function Home() {
       <Keyword />   
       <Features />
       <Features05 />
-      {/* <Features02 /> */}
-      {/* <Features03 /> */}
-      {/* <TestimonialsCarousel /> */}
-      {/* <Features04 /> */}
-      {/* <Pricing /> */}
-      {/* <Customers />
-      <Faqs />
-      <Testimonials />
-      <Cta />  */}
-
     </>
   )
 }
